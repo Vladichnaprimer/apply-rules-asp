70df7b0e34a781baf126e316b3c05cd8141316ea
Zhenya Deryaka <Zhenya Deryaka>
2017-03-13 23:32:48 +0200

    Added create action

--- ApplyNureRulesApp/Controllers/DashboardController.cs
+++ ApplyNureRulesApp/Controllers/DashboardController.cs
@@ -3,22 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ApplyNureRulesApp.Models;
+using System.Web.UI.HtmlControls;
 
 namespace ApplyNureRulesApp.Controllers
 {
     public class DashboardController : Controller
     {
+        ApplyRulesDBEntities db = new Models.ApplyRulesDBEntities();
+
         // GET: Dashboard
         [Authorize]
         public ActionResult Index()
         {
-            return View();
+            //var content = (from cont in db.Contents
+            //               join us in db.UserCategories
+            //                on cont.CategoryId equals us.Id
+            //               select new { cont.Title, cont.Description, catTitile = us.Title }).ToList();
+
+            var content = (from cont in db.Contents select cont).ToList();
+            return View(content);
         }
 
         [Authorize]
         public ActionResult Add()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(FormCollection form)
+        {
+            string title = form["Title"].ToString();
+            string description = form["Description"].ToString();
+
+            //TODO add checking list items
+
+            Content content = new Content();
+
+            content.AdminId = 1;
+            content.CategoryId = 1;
+            content.Description = description;
+            content.Title = title;
+
+
+            db.Contents.Add(content);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
